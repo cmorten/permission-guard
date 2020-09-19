7022d371eb7e42fb019889d36091e4bc359bffd1
asos-craigmorten <craig.morten@asos.com>
2020-09-19 15:33:19 +0100

    test: add revoke tests

--- test/guard.test.ts
+++ test/guard.test.ts
@@ -14,69 +14,208 @@ import { expect } from "./deps.ts";
 const denoRunCommandPrefix = ["deno", "run", "--unstable"];
 
 describe("guard", () => {
-  describe("when no grants are requested", () => {
-    const scriptPath = "./examples/defaults/index.ts";
+  describe("when the guard doesn't revoke undesired permissions", () => {
+    describe("when no grants are requested", () => {
+      const scriptPath = "./examples/defaults/index.ts";
 
-    describe("when no permission flags are provided", () => {
-      it("should exit with status 0", async () => {
-        const process = await Deno.run({
-          cmd: [...denoRunCommandPrefix, scriptPath],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when no permission flags are provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [...denoRunCommandPrefix, scriptPath],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 0, success: true });
-
-        await process.close();
       });
-    });
 
-    describe("when a top-level permission is provided", () => {
-      it("should exit with status 1", async () => {
-        const process = await Deno.run({
-          cmd: [
-            ...denoRunCommandPrefix,
-            "--allow-env",
-            scriptPath,
-          ],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when a top-level permission is provided", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 1, success: false });
-
-        await process.close();
       });
-    });
 
-    describe("when the insecure all permissions flag is provided", () => {
-      it("should exit with status 1", async () => {
-        const process = await Deno.run({
-          cmd: [
-            ...denoRunCommandPrefix,
-            "-A",
-            scriptPath,
-          ],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when the insecure all permissions flag is provided", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "-A",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 1, success: false });
-
-        await process.close();
       });
-    });
 
-    /**
+      /**
      * Not out of choice, we simply cannot check for this atm. But at least we 
      * can ensure we don't error!
      */
-    describe("when a scoped permission is provided", () => {
-      it("should exit with status 0", async () => {
+      describe("when a scoped permission is provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-read=/secret",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
+        });
+      });
+    });
+
+    describe("when grants are requested", () => {
+      const scriptPath = "./examples/granted-strict/index.ts";
+
+      describe("when only the requested permission flags are provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              "--allow-net=google.com",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
+        });
+      });
+
+      describe("when a top-level permission flag is missing", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-net=google.com",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
+        });
+      });
+
+      describe("when a scoped permission flag is missing", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [...denoRunCommandPrefix, "--allow-env", scriptPath],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
+        });
+      });
+
+      describe("when an unnecessary insecure top-level permission flag is provided", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              "--allow-net=google.com",
+              "--allow-write",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
+        });
+      });
+
+      describe("when the insecure all permissions flag is provided", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "-A",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
+        });
+      });
+
+      /**
+     * Not out of choice, we simply cannot check for this atm. But at least we 
+     * can ensure we don't error!
+     */
+      describe("when an insecure scoped permission flag is provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              "--allow-net=google.com",
+              "--allow-write=/secret",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
+        });
+      });
+    });
+
+    describe("when a top-level grant is requested and the permission supports allowlisting", () => {
+      const scriptPath = "./examples/granted-recommend-allowlist/index.ts";
+
+      it("should log a recommendation and exit with exit code 0", async () => {
         const process = await Deno.run({
           cmd: [
             ...denoRunCommandPrefix,
-            "--allow-read=/secret",
+            "--allow-net",
             scriptPath,
           ],
           stdout: "inherit",
@@ -90,136 +229,122 @@ describe("guard", () => {
     });
   });
 
-  describe("when grants are requested", () => {
-    const scriptPath = "./examples/granted-strict/index.ts";
+  describe("when the guard revokes undesired permissions", () => {
+    describe("when grants are requested", () => {
+      const scriptPath = "./examples/granted-strict-with-revoke/index.ts";
 
-    describe("when only the requested permission flags are provided", () => {
-      it("should exit with status 0", async () => {
-        const process = await Deno.run({
-          cmd: [
-            ...denoRunCommandPrefix,
-            "--allow-env",
-            "--allow-net=google.com",
-            scriptPath,
-          ],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when only the requested permission flags are provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              "--allow-net=google.com",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 0, success: true });
-
-        await process.close();
       });
-    });
 
-    describe("when a top-level permission flag is missing", () => {
-      it("should exit with status 1", async () => {
-        const process = await Deno.run({
-          cmd: [...denoRunCommandPrefix, "--allow-net=google.com", scriptPath],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when a top-level permission flag is missing", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-net=google.com",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 1, success: false });
-
-        await process.close();
       });
-    });
 
-    describe("when a scoped permission flag is missing", () => {
-      it("should exit with status 1", async () => {
-        const process = await Deno.run({
-          cmd: [...denoRunCommandPrefix, "--allow-env", scriptPath],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when a scoped permission flag is missing", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [...denoRunCommandPrefix, "--allow-env", scriptPath],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 1, success: false });
-
-        await process.close();
       });
-    });
 
-    describe("when an unnecessary insecure top-level permission flag is provided", () => {
-      it("should exit with status 1", async () => {
-        const process = await Deno.run({
-          cmd: [
-            ...denoRunCommandPrefix,
-            "--allow-env",
-            "--allow-net=google.com",
-            "--allow-write",
-            scriptPath,
-          ],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when an unnecessary insecure top-level permission flag is provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              "--allow-net=google.com",
+              "--allow-write",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 1, success: false });
-
-        await process.close();
       });
-    });
 
-    describe("when the insecure all permissions flag is provided", () => {
-      it("should exit with status 1", async () => {
-        const process = await Deno.run({
-          cmd: [
-            ...denoRunCommandPrefix,
-            "-A",
-            scriptPath,
-          ],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when the insecure all permissions flag is provided", () => {
+        it("should exit with status 1", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "-A",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 1, success: false });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 1, success: false });
-
-        await process.close();
       });
-    });
 
-    /**
+      /**
      * Not out of choice, we simply cannot check for this atm. But at least we 
      * can ensure we don't error!
      */
-    describe("when an insecure scoped permission flag is provided", () => {
-      it("should exit with status 0", async () => {
-        const process = await Deno.run({
-          cmd: [
-            ...denoRunCommandPrefix,
-            "--allow-env",
-            "--allow-net=google.com",
-            "--allow-write=/secret",
-            scriptPath,
-          ],
-          stdout: "inherit",
-          stderr: "inherit",
+      describe("when an insecure scoped permission flag is provided", () => {
+        it("should exit with status 0", async () => {
+          const process = await Deno.run({
+            cmd: [
+              ...denoRunCommandPrefix,
+              "--allow-env",
+              "--allow-net=google.com,bad.net",
+              "--allow-write=/secret",
+              scriptPath,
+            ],
+            stdout: "inherit",
+            stderr: "inherit",
+          });
+
+          expect(await process.status()).toEqual({ code: 0, success: true });
+
+          await process.close();
         });
-
-        expect(await process.status()).toEqual({ code: 0, success: true });
-
-        await process.close();
       });
-    });
-  });
-
-  describe("when a top-level grant is requested and the permission supports allowlisting", () => {
-    const scriptPath = "./examples/granted-recommend-allowlist/index.ts";
-
-    it("should log a recommendation and exit with exit code 0", async () => {
-      const process = await Deno.run({
-        cmd: [
-          ...denoRunCommandPrefix,
-          "--allow-net",
-          scriptPath,
-        ],
-        stdout: "inherit",
-        stderr: "inherit",
-      });
-
-      expect(await process.status()).toEqual({ code: 0, success: true });
-
-      await process.close();
     });
   });
 });
